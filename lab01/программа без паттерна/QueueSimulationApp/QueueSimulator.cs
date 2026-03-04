feee623d7f5c202fbb4c9ad3a9a5c8fb7a6d04dc
MIDDY <vsl102016@gmail.com>
2026-03-04 10:58:53 +0700

    Update QueueSimulator.cs

--- lab01/программа без паттерна/QueueSimulationApp/QueueSimulator.cs
+++ lab01/программа без паттерна/QueueSimulationApp/QueueSimulator.cs
@@ -195,5 +195,11 @@ namespace QueueSimulationApp
                 OnEvent?.Invoke("Моделирование остановлено пользователем");
             }
         }
+
+         public interface IMyChannel<T>
+        {
+            T Clone();
+        }
     }
+
 }
